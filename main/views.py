ab89d1d8ff7f25e36c9e69b6eeb2242a0ad79e5f
clever88 <prostopavel2020@yandex.ru>
2022-09-26 22:56:08 +0300

    creating a website structure

--- main/views.py
+++ main/views.py
@@ -1,14 +1,14 @@
-#from django.shortcuts import render
+from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
 
 
 def index(request):
-    return HttpResponse ("<h4>проверка работа</h4>")
+    return render(request, 'main/index.html' )
 
 def login(request):
-    return HttpResponse ("<h4>тип логин</h4>")
+    return render(request, 'main/about.html' )
 
 def admin(request):
     return HttpResponse ("<h4>тип админка</h4>")
