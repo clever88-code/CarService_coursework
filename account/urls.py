eb169a255fc5ec77bfa7b15f86675150830ec8d6
clever888 <prostopavel2020@yandex.ru>
2022-10-19 02:43:26 -0400

    app additions accounts

---
+++ account/urls.py
@@ -0,0 +1,7 @@
+from argparse import ArgumentDefaultsHelpFormatter
+from django.contrib import admin
+from django.urls import path, include
+from django.contrib.auth.views import LoginView
+from . import views
+
+
