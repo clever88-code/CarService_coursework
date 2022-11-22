7fbadd439dd5970986cd24d6ec200114688e2d85
Bogdan <bs@24h.tv>
2022-11-22 13:46:42 +0300

    done

--- CarService/urls.py
+++ CarService/urls.py
@@ -6,14 +6,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 
-
-
-
 urlpatterns = [
-    #приложения main
-    path('', include('main.urls') ),
-    #приложения record
-    path('record/', include('record.urls') ),
-    
+    path('', include('main.urls')),
+    path('record/', include('record.urls')),
     path('admin/', admin.site.urls)
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
