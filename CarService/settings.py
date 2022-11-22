966ea3e6fe684a2f2259b5dae4bc5fb0d6addd5a
Vladislav Aganin <vlad_aganin33@hotmail.com>
2022-11-22 12:28:09 +0300

    Forms fix

--- CarService/settings.py
+++ CarService/settings.py
@@ -5,7 +5,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
@@ -17,11 +16,10 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = [
-    
+
     'main',
     'record',
     'core',
@@ -32,21 +30,16 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'crispy_forms'
-    
+
 ]
 
-
-#перенаправления пользователей после авториризации
+# перенаправления пользователей после авториризации
 LOGIN_REDIRECT_URL = "home"
-#перенаправления пользователей после выхода
+# перенаправления пользователей после выхода
 LOGOUT_REDIRECT_URL = 'home'
 # библиотека для форм
 
-CRISPY_TEMPLATE_PACK = 'uni_form'
-
-
-
-
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -78,7 +71,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'CarService.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
@@ -88,7 +80,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
@@ -108,7 +99,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
@@ -120,19 +110,15 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
 
-
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
 
 STATICFILES_DIRS = [
     BASE_DIR / "static"
